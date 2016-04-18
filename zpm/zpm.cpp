c95b67f7d70c69e8051eb5b32f86240fb6242bd7
Caroline Danzi <danzicr@miamioh.edu>
2016-04-18 19:34:44 -0400

    C++ zpm can now read in a file and print the lines

--- zpm/zpm.cpp
+++ zpm/zpm.cpp
@@ -6,10 +6,52 @@
 // A is the thing to print (variable, int, or String).  Assignment statements have the 
 // form A = B ; A += B ; A -= B ; A *= B ; where only = and += work for Strings.
 
-#include <iostream>
-#include <stdlib.h>
+#include "zpm.h"
+#include <fstream>
 
-class zpm
+zpm::zpm()
 {
+	
+}
 
+zpm::~zpm()
+{
+	
+}
+
+void zpm::parseFile(char* fileName)
+{
+	// Open file for reading
+	std::ifstream sourceFile;
+	sourceFile.open(fileName);
+	
+	// Loop through the lines in the file and parse each statement
+	std::string line = "";
+	while(!sourceFile.eof())
+	{
+		getline(sourceFile, line);
+		parseStmt(line);
+	}
+}
+
+void zpm::parseStmt(std::string line)
+{
+	std::cout << line << std::endl;
+}
+
+int main(int argc, char* argv[])
+{
+	if(argc < 2)
+	{
+		std::cerr << "Expected a command line arguments" << std::endl;
+	}
+	
+	// The first command line argument is the name of the file,
+	// the second is the source code file of the program we are interpreting
+	char* sourceFileName = argv[1];
+	
+	zpm* interpreter = new zpm();
+	interpreter->parseFile(sourceFileName);
+	
+	return 0;
 }
---
+++ zpm/zpm.h
@@ -0,0 +1,35 @@
+// Caroline Danzi
+// Dr. Zmuda
+// CSE 465
+// Z+- Problem
+
+#include <map>
+#include <iostream>
+#include <stdlib.h>
+
+class zpm
+{
+	private:
+		std::map<std::string, std::string> variables;
+		int lineNum;
+	
+	public:
+		// Constructor
+		zpm();
+		
+		// Destructor
+		~zpm();
+		
+		void parseFile(char* fileName);
+		
+		// Takes in a line of the source code, parses it,
+		// and sends it to the proper function
+		void parseStmt(std::string line);
+		
+		// 
+		void analyzeAssignment(std::string line);
+		
+		void analyzePrint(std::string line);
+		
+		void raiseError();
+};
