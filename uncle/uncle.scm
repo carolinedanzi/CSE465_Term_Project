605c7ad44d1e02dd6f0e68f67f608a6210e9e536
Caroline Danzi <danzicr@miamioh.edu>
2016-04-28 20:32:04 -0400

    Added test cases and comments to scheme great uncle

--- uncle/uncle.scm
+++ uncle/uncle.scm
@@ -4,6 +4,7 @@
 ; Starter file from Dr. Zmuda
 ; Great Uncle/Aunt Problem
 
+; A list of the parent relationships (parent child)
 (define parents '((male1 male2)(male1 male3)(male1 female2)
 (female1 male2)(female1 male3)(female1 female2)(male2 male4)
 (male2 male8)(female3 male4)(female3 male8)(male4 female7)
@@ -11,16 +12,23 @@
 (female8 female9)(male3 female5)(male3 male5)(female4 female5)
 (female4 male5)))
 
+; A list of all the people who are married (husband wife)
 (define married '((male1 female1)(male2 female3)(male3 female4)
 (female2 male6)(male4 female6)(male8 female8)(male7 female10)))
 
+; Returns true if uncle is a great uncle or aunt to kid
 (define (greatUncle lst uncle kid)
+	; Overall structure: find the parents of the uncle and their spouse, then check the 
+	; third generation down to see if the kid is there
 	(if (member kid (getNextGeneration parents 
 	(getNextGeneration parents 
+	; Note: remove the uncle and their spouse from the first generation we get 
+	; First generation from great uncle's parents is the great uncle and their siblings
 	(remove (getSpouse married uncle)
 	(remove uncle (getNextGeneration parents (getAuntUncleParents lst married uncle))))))) #t #f)
 )
 
+; Gets all the parents of the great uncle and their spouse
 (define (getAuntUncleParents parents married uncle)
 	; append the list of the uncle's parents to the aunt's parents
 	(append (getParents parents uncle) (getParents parents (getSpouse married uncle)))
@@ -31,6 +39,7 @@
 (define (getSpouse married person)
 	(COND
 		((NULL? married) #f)
+		; Check both elements in each marriage pair
 		((EQUAL? (cadar married) person) (caar married))
 		((EQUAL? (caar married) person) (cadar married))
 		(else (getSpouse (cdr married) person))
@@ -57,6 +66,7 @@
 	)
 )
 
+; Removes element from list (used to remove great uncle from list of siblings)
 (define (remove elmt lst)
 	(COND
 		((NULL? lst) '())
@@ -65,6 +75,7 @@
 	)
 )
 
+; Gets all the children of all the people in the current generation list
 (define (getNextGeneration parents currentGen)
 	(COND
 		((NULL? currentGen) '())
@@ -72,19 +83,28 @@
 	)
 )
 
-(display (getKids parents 'male6))
 (newline)
-(display (getKids parents 'female6))
+(display (greatUncle parents 'male6 'female7)) ; #t
 (newline)
-(display (getParents parents 'female2))
+(display (greatUncle parents 'male6 'male8)) ; #f
 (newline)
-(display (greatUncle parents 'male3 'male7))
+(display (greatUncle parents 'female7 'male3)) ; #f
 (newline)
-(display (greatUncle parents 'b 'b))
+(display (greatUncle parents 'female4 'female7)) ; #t
 (newline)
-(display (greatUncle parents 'c 'd))
+(display (greatUncle parents 'female4 'male3)) ; #f
 (newline)
-(display (greatUncle parents 'd 'c))
+(display (greatUncle parents 'male3 'male7)) ; #t
+(newline)
+(display (greatUncle parents 'male6 'female7)) ; #t
+(newline)
+(display (greatUncle parents 'female7 'male6)) ; #f
+(newline)
+(display (greatUncle parents 'male5 'female8)) ; #f
+(newline)
+(display (greatUncle parents 'male5 'male8)) ; #f
+(newline)
+(display (greatUncle parents 'male8 'female8)) ; #f
 (newline)
 
 ,exit
